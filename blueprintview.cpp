cdaacf4fe1365be18f50390b01326e48bd3ba085
Andrey Matveyakin <a.matveyakin@gmail.com>
2014-04-10 01:08:04 +0400

    Preserve scroll position on zoom and resize

--- blueprintview.cpp
+++ blueprintview.cpp
@@ -1,9 +1,17 @@
+#include <assert.h>
+
 #include <QPainter>
 #include <QScrollBar>
 #include <QWheelEvent>
 
 #include "blueprint.h"
 #include "blueprintview.h"
+
+
+static inline double rectToRectSizeCoeff(const QRect& rect1, const QRect& rect2)
+{
+    return qMin(double(rect2.width()) / rect1.width(), double(rect2.height()) / rect1.height());
+}
 
 
 BlueprintView::BlueprintView(QWidget *parentArg)
@@ -18,21 +26,19 @@ BlueprintView::BlueprintView(QWidget *parentArg)
 void BlueprintView::setBlueprint(const Blueprint* blueprint)
 {
     m_blueprint = blueprint;
-    updateAll();
+    m_scale = 1.;
+    updateViewportGeometry();
+    coLocatePoints(blueprintRect().center(), m_canvasRect.center());
 }
 
 void BlueprintView::paintEvent(QPaintEvent*)
 {
     QPainter painter(viewport());
-    QRect viewportRect = viewport()->rect();
 //    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing | QPainter::SmoothPixmapTransform | QPainter::HighQualityAntialiasing);
-    painter.fillRect(viewportRect, Qt::white);
+    painter.fillRect(viewport()->rect(), Qt::white);
     if (!m_blueprint)
         return;
-    painter.translate(canvasRect().left(), canvasRect().top());
-    painter.translate(-horizontalScrollBar()->value(), -verticalScrollBar()->value());
-    painter.scale(sizeCoeff(), sizeCoeff());
-    painter.translate(-blueprintBounds().left(), -blueprintBounds().top());
+    painter.setTransform(blueprintToScreenTransform());
     painter.setBrush(Qt::NoBrush);
     for (const Element& element : m_blueprint->elements()) {
         painter.setPen(QPen(Qt::black, element.width, Qt::SolidLine, Qt::FlatCap, Qt::BevelJoin));  // TODO: PenCapStyle, PenJoinStyle - ?
@@ -42,31 +48,52 @@ void BlueprintView::paintEvent(QPaintEvent*)
 
 void BlueprintView::wheelEvent(QWheelEvent* ev)
 {
+    if (!m_blueprint)
+        return;
     const double factorBase = 1.001;
     double factor = pow(factorBase, ev->delta());
     zoom(factor, ev->pos());
 }
 
-void BlueprintView::resizeEvent(QResizeEvent*)
+void BlueprintView::resizeEvent(QResizeEvent* /*ev*/)
 {
-    updateAll();
+    if (!m_blueprint)
+        return;
+    QPoint blueprintFixedPoint = screenToBlueprint(m_canvasRect.center());
+    updateViewportGeometry();
+    QPoint goalScreenPoint = m_canvasRect.center();
+    coLocatePoints(blueprintFixedPoint, goalScreenPoint);
 }
 
-QRect BlueprintView::blueprintBounds() const
+QTransform BlueprintView::blueprintToScreenTransform() const
 {
-    return m_blueprint->boundingRect();
+    QTransform result;
+    result.translate(m_canvasRect.center().x(), m_canvasRect.center().y());
+    result.translate(horizontalScrollBar()->maximum() / 2 - horizontalScrollBar()->value(),
+                     verticalScrollBar()->maximum() / 2 - verticalScrollBar()->value());
+    result.scale(sizeCoeff(), sizeCoeff());
+    result.translate(-blueprintRect().center().x(), -blueprintRect().center().y());
+    return result;
 }
 
-QRect BlueprintView::canvasRect() const
+QPoint BlueprintView::screenToBlueprint(QPoint pos) const
 {
-    const int canvasMargin = 8;
-    QRect viewportRect = viewport()->rect();
-    return viewportRect.adjusted(canvasMargin, canvasMargin, -canvasMargin, -canvasMargin);
+    return blueprintToScreenTransform().inverted().map(pos);
+}
+
+QPoint BlueprintView::blueprintToScreen(QPoint pos) const
+{
+    return blueprintToScreenTransform().map(pos);
+}
+
+QRect BlueprintView::blueprintRect() const
+{
+    return m_blueprint ? m_blueprint->boundingRect() : QRect();
 }
 
 double BlueprintView::builtInSizeCoeff() const
 {
-    return qMin(double(canvasRect().width()) / blueprintBounds().width(), double(canvasRect().height()) / blueprintBounds().height());
+    return rectToRectSizeCoeff(blueprintRect(), m_canvasRect);
 }
 
 double BlueprintView::sizeCoeff() const
@@ -74,28 +101,45 @@ double BlueprintView::sizeCoeff() const
     return  builtInSizeCoeff() * m_scale;
 }
 
-void BlueprintView::zoom(double factor, QPoint /*fixedPoint*/)
+void BlueprintView::zoom(double factor, QPoint fixedScreenPoint)
 {
-    // TODO: preserve fixed point
+    QPoint fixedBlueprintPoint = screenToBlueprint(fixedScreenPoint);
     m_scale = qMax(m_scale * factor, 1.);
-    updateAll();
+    updateViewportGeometry();
+    coLocatePoints(fixedBlueprintPoint, fixedScreenPoint);
 }
 
-void BlueprintView::updateScrollBars()
+void BlueprintView::updateCanvasRect()
+{
+    // TODO: Fix negative rects
+    const int canvasMargin = 8;
+    m_canvasRect = viewport()->rect().adjusted(canvasMargin, canvasMargin, -canvasMargin, -canvasMargin);
+}
+
+void BlueprintView::updateScrollBarRanges()
 {
     if (!m_blueprint) {
         horizontalScrollBar()->setMaximum(0);
         verticalScrollBar()->setMaximum(0);
         return;
     }
-    horizontalScrollBar()->setPageStep(canvasRect().width());
-    verticalScrollBar()->setPageStep(canvasRect().height());
-    horizontalScrollBar()->setMaximum(qMax(0., blueprintBounds().width() * sizeCoeff() - canvasRect().width()));
-    verticalScrollBar()->setMaximum(qMax(0., blueprintBounds().height() * sizeCoeff() - canvasRect().height()));
+    horizontalScrollBar()->setPageStep(m_canvasRect.width());
+    verticalScrollBar()->setPageStep(m_canvasRect.height());
+    horizontalScrollBar()->setMaximum(qMax(0., blueprintRect().width() * sizeCoeff() - m_canvasRect.width()));
+    verticalScrollBar()->setMaximum(qMax(0., blueprintRect().height() * sizeCoeff() - m_canvasRect.height()));
 }
 
-void BlueprintView::updateAll()
+void BlueprintView::updateViewportGeometry()
 {
-    updateScrollBars();
+    updateCanvasRect();
+    updateScrollBarRanges();
+    viewport()->update();
+}
+
+void BlueprintView::coLocatePoints(QPoint blueprintPoint, QPoint screenPoint)
+{
+    QPoint scroll = blueprintToScreen(blueprintPoint) - screenPoint;
+    horizontalScrollBar()->setValue(horizontalScrollBar()->value() + scroll.x());
+    verticalScrollBar()->setValue(verticalScrollBar()->value() + scroll.y());
     viewport()->update();
 }
--- blueprintview.h
+++ blueprintview.h
@@ -23,16 +23,21 @@ protected:
 
 private:
     const Blueprint* m_blueprint;
+    QRect m_canvasRect;
     double m_scale;
 
 private:
-    QRect blueprintBounds() const;
-    QRect canvasRect() const;
+    QTransform blueprintToScreenTransform() const;
+    QPoint screenToBlueprint(QPoint pos) const;
+    QPoint blueprintToScreen(QPoint pos) const;
+    QRect blueprintRect() const;
     double builtInSizeCoeff() const;
     double sizeCoeff() const;
-    void zoom(double factor, QPoint fixedPoint);
-    void updateScrollBars();
-    void updateAll();
+    void zoom(double factor, QPoint fixedScreenPoint);
+    void updateCanvasRect();
+    void updateScrollBarRanges();
+    void updateViewportGeometry();
+    void coLocatePoints(QPoint blueprintPoint, QPoint screenPoint);     ///< update geometry and co-locate specified point together
 };
 
 #endif // BLUEPRINTVIEW_H
