7c685474a5f892ad5636b30718f22f32a75ca90f
Andrey Matveyakin <a.matveyakin@gmail.com>
2014-06-28 14:59:58 +0400

    Update version

--- appinfo.cpp
+++ appinfo.cpp
@@ -15,7 +15,7 @@ QString companyName()
 QList<int> appVersion()
 {
   // TODO: extract version from git!
-  return QList<int>() << 1 << 0;
+  return QList<int>() << 1 << 1;
 }
 
 QString appVersionString()
